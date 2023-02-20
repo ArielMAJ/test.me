0d97286daf441ab84d58e057149a74776c8540ba
ArielMAJ <ariel.maj@hotmail.com>
2023-02-20 16:04:56 -0300

    [UPT/RMV] [Task 105] Removed duplicate solution and updated solution.
    Co-authored-by: tauanesales <taysales6@gmail.com>

--- example_input/Q01/solution.c
+++ example_input/Q01/solution.c
@@ -7,22 +7,22 @@ int main(void)
 	int n_linhas;
 	scanf("%d", &n_linhas);
 
-	int n1 = 2, n2 = 3;
+	for (int i = 0; i < n_linhas; ++i)
+	{
+		int n1, n2;
 
-	scanf("%d", &n1);
-	scanf("%d", &n2);
+		scanf("%d", &n1);
+		scanf("%d", &n2);
 
-	printf("%d\n", multiplica(n1, n2));
+		printf("%d\n", multiplica(n1, n2));
+	}
 	return 0;
 }
 
 int multiplica(int a, int b)
 {
 	int resultado = 0;
-	for(int i = 0; i < b; i++)
-	{
+	for (int i = 0; i < b; i++)
 		resultado += a;
-	}
 	return resultado;
 }
-
